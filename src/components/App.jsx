3a4ce6d23ca535eea7a9c24d3495b8a50dc3eca7
m-hatch <montehatch@gmail.com>
2018-06-03 19:45:12 -0400

    set up web socket

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,51 @@ export default class App extends React.Component {
 
   constructor() {
     super();
-    this.state = { 
-      
-    };
+    this.socket;
+    this.state = {};
+    this.createWebSocket = this.createWebSocket.bind(this);
   }
 
   componentDidMount() {
+    this.createWebSocket();
+  }
+
+  createWebSocket() {
+    let socket = new WebSocket('wss://www.bitmex.com/realtime?subscribe=trade');
+    socket.onerror = this.onError;
+    socket.onopen = this.onOpen;
+    socket.onclose = this.onClose;
+    socket.onmessage = this.onMessage;
+    this.socket = socket;
+  }
+
+  onOpen(event) {
+    console.log('Connected to: ' + event.currentTarget.url);
+  }
+
+  onMessage(event) {
+    const message = JSON.parse(event.data);
+    const data = message.data;
+    console.log(message.data);
+  }
+
+  onClose(event) {
+    console.log('Disconnected from WebSocket.');
+  }
+
+  onError(error) {
+    console.log('WebSocket Error: ' + error);
   }
 
   render() {
     return (
       <div className="app">
-        Hello Crypto
+
+        <div className="header">
+          <h1 className="header__title">Realtime Cryptocurrency Trade Ticker</h1>
+          <span id="js-status">Connecting...</span>
+        </div>
+
       </div>
     );
   }
