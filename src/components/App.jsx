675491b92c636744f68dac171360f2a46b81d97d
m-hatch <montehatch@gmail.com>
2018-06-03 19:02:34 -0400

    add configuration

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { 
+      
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div className="app">
+        Hello Crypto
+      </div>
+    );
+  }
+  
+}
