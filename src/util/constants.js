f870ee4043f1ca1be36439e033caca788ac7c8d7
m-hatch <hatch@hq.acm.org>
2018-06-04 14:52:48 -0400

    create gh-pages build

--- src/util/constants.js
+++
@@ -1,9 +0,0 @@
-// Cryptocurrency Trade Symbols
-export const SYMBOLS = {
-  xbt: ['XBTUSD', 'XBTM18', 'XBTU18', 'XBT7D_D90', 'XBT7D_U110', '.BXBT', '.BXBT30M'],
-  bch: ['BCHM18', 'BCHXBT', 'BCHXBT30M'],
-  ada: ['ADAM18', 'ADAXBT', 'ADAXBT30M'],
-  eth: ['ETHM18', '.ETHBON', '.ETHBON2H', '.ETHBON8H', '.ETHXBT', '.ETHXBT30M'],
-  ltc: ['LTCM18', 'LTCXBT', 'LTCXBT30M'],
-  xrp: ['XRPM18', 'XRPMXBT', 'XRPMXBT30M']
-}
--- src/util/utilities.js
+++
@@ -1,8 +0,0 @@
-export const formatPrice = (price, curr) => {
-  return (price && curr === 'USD') ? price.toFixed(2) : price;
-}
-
-export const getTrend = (tick) => {
-  return (tick === 'MinusTick' || tick === 'ZeroMinusTick') ? 
-  'bear' : 'bull';
-}
