2609c4e47ab1151262c9bbc3b6f082628b658004
m-hatch <hatch@hq.acm.org>
2018-06-04 10:57:28 -0400

    push trades to array

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,34 +8,40 @@ export default class Dashboard extends React.Component {
 
   constructor() {
     super();
+
+    // initialize state
     this.state = {
-      xbt: null,
-      bch: null,
-      ada: null,
-      eth: null,
-      ltc: null,
-      xrp: null
+      xbt: { name: 'Bitcoin (XBT)', curr: 'USD', trades: [] },
+      bch: { name: 'Bitcoin Cash (BCH)', curr: 'BTC', trades: [] },
+      ada: { name: 'Cardano (ADA)', curr: 'BTC', trades: [] },
+      eth: { name: 'Ethereum (ETH)', curr: 'BTC', trades: [] },
+      ltc: { name: 'Litecoin (LTC)', curr: 'BTC', trades: [] },
+      xrp: { name: 'Ripple (XRP)', curr: 'BTC', trades: [] }
     };
-    this.sortMessage = this.sortMessage.bind(this);
+
+    // bind methods
+    this.processMessage = this.processMessage.bind(this);
     this.sortTrades = this.sortTrades.bind(this);
+    this.addTrade = this.addTrade.bind(this);
+    this.getPanels = this.getPanels.bind(this);
   }
 
   componentDidMount() {
     // only needed if you start with an initial message
     if (this.props.message) {
-      this.sortMessage(this.props.message);
+      this.processMessage(this.props.message);
     }
   }
 
   componentDidUpdate(previousProps) {
     // prevent re-render if props haven't changed
     if (previousProps.message !== this.props.message) {
-      this.sortMessage(this.props.message);
+      this.processMessage(this.props.message);
       console.log(this.state)
     }
   }
 
-  sortMessage(message) {
+  processMessage(message) {
     message.forEach(trade => {
       this.sortTrades(trade);
     });
@@ -43,25 +49,35 @@ export default class Dashboard extends React.Component {
 
   sortTrades(trade) {
     Object.keys(SYMBOLS).forEach(key => {
-
       if (SYMBOLS[key].includes(trade.symbol)) {
-        this.setState({
-          [key]: trade
-        });
+        this.addTrade(key, trade);
       }
-
     });
+  }
+
+  addTrade(key, trade) {
+    const copy = this.state[key];
+
+    if (copy.trades.length > 19) { 
+      copy.trades.shift() 
+    }
+
+    copy.trades.push(trade);
+    this.setState({ [key]: copy });
+  }
+
+  getPanels() {
+    return Object.keys(this.state).map(key => (
+      <div className="panel" key={ key }>
+        { this.state[key].name + ' ' + this.state[key].curr}
+      </div>
+    ));
   }
 
   render() {
     return (
       <div className="dashboard">
-        <div className="panel"></div>
-        <div className="panel"></div>
-        <div className="panel"></div>
-        <div className="panel"></div>
-        <div className="panel"></div>
-        <div className="panel"></div>
+        { this.getPanels() }
       </div>
     );
   }
